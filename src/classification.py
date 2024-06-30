8613c2c9810537473765d2e001f24aa994d35552
unknown <2569618533@qq.com>
2024-06-30 00:03:55 -0700

    update compute dtype

--- src/classification.py
+++ src/classification.py
@@ -10,7 +10,8 @@ from transformers import (
     AutoTokenizer,
     Trainer,
     TrainingArguments,
-    get_cosine_schedule_with_warmup
+    get_cosine_schedule_with_warmup,
+    DataCollatorWithPadding
 )
 from datasets import Dataset
 from sklearn.metrics import log_loss
@@ -41,11 +42,8 @@ class CustomTrainer(Trainer):
 
 def compute_metrics(p):
     logits = p.predictions
-    print(logits)
     labels = p.label_ids
-    print(labels)
     probabilities = softmax(logits, axis=-1)
-    print(probabilities)
 
     return {"log_loss": log_loss(labels, probabilities)}
 
@@ -58,19 +56,17 @@ def train(args):
         add_eos_token=True,
         token=args.huggingface_api
     )
-    if "Llama" in args.model_name:
-        tokenizer.add_special_tokens({"pad_token": "<pad>"})
 
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_compute_dtype=torch.bfloat16,
         bnb_4bit_use_double_quant=True
     )
     model = AutoModelForSequenceClassification.from_pretrained(
         args.model_name,
         num_labels=3,
-        torch_dtype=torch.float16,
+        torch_dtype=torch.bfloat16,
         quantization_config=bnb_config,
         device_map="auto",
         trust_remote_code=True,
@@ -81,13 +77,19 @@ def train(args):
     try:
         model.config.pad_token_id = tokenizer.pad_token_id
     except:
+        print("Set eos_token as pad_token !!!")
         tokenizer.pad_token = tokenizer.eos_token
         model.config.pad_token_id = tokenizer.pad_token_id
-    # model.resize_token_embeddings(len(tokenizer))
-    print("New tokenizer length:", len(tokenizer))
+    
+    print("New Tokenizer Length:", len(tokenizer))
 
-    model.gradient_checkpointing_enable()
-    model = prepare_model_for_kbit_training(model)
+    if bnb_config.bnb_4bit_compute_dtype == torch.float16:
+        model.config.use_cache = False
+        model.gradient_checkpointing_enable()
+        model = prepare_model_for_kbit_training(model)
+    elif bnb_config.bnb_4bit_compute_dtype == torch.bfloat16:
+        model.config.use_cache = False
+        model.gradient_checkpointing_enable()
 
     peft_config = LoraConfig(
         task_type=TaskType.SEQ_CLS,
@@ -108,8 +110,8 @@ def train(args):
         return tokenizer(sample["input"], max_length=args.max_length, truncation=True)
 
     train, val = classification_data_preprocessing(args.train_file_path)
-    train["input"] = train["full_chat_a"] + "########" + train["full_chat_b"]
-    val["input"] = val["full_chat_a"] + "########" + val["full_chat_b"]
+    train["input"] = train["full_chat_a"] + "\n####\n" + train["full_chat_b"]
+    val["input"] = val["full_chat_a"] + "\n####\n" + val["full_chat_b"]
 
     ds_train = Dataset.from_pandas(train)
     ds_val = Dataset.from_pandas(val)
@@ -121,23 +123,25 @@ def train(args):
         ["id", "prompt", "response_a", "response_b", "full_chat_a", "full_chat_b"]
     )
 
-    class DataCollator:
-        def __call__(self, features):
-            model_inputs = [
-                {
-                    "input_ids": feature["input_ids"],
-                    "attention_mask": feature["attention_mask"],
-                    "labels": feature["labels"]
-                } for feature in features
-            ]
-            batch = tokenizer.pad(
-                model_inputs,
-                padding="max_length",
-                max_length=args.max_length,
-                return_tensors="pt",
-                pad_to_multiple_of=8
-            )
-            return batch
+    # class DataCollator:
+    #     def __call__(self, features):
+    #         model_inputs = [
+    #             {
+    #                 "input_ids": feature["input_ids"],
+    #                 "attention_mask": feature["attention_mask"],
+    #                 "labels": feature["labels"]
+    #             } for feature in features
+    #         ]
+    #         batch = tokenizer.pad(
+    #             model_inputs,
+    #             padding="max_length",
+    #             max_length=args.max_length,
+    #             return_tensors="pt",
+    #             pad_to_multiple_of=8
+    #         )
+    #         return batch
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     training_args = TrainingArguments(
         per_device_train_batch_size=args.per_device_train_batch_size,
@@ -157,27 +161,27 @@ def train(args):
         save_steps=args.save_steps,
         load_best_model_at_end=True,
         metric_for_best_model="log_loss",
-        lr_scheduler_type="cosine",
         weight_decay=args.weight_decay,
         save_total_limit=args.save_total_limit
     )
 
     optimizer = AdamW8bit(
         model.parameters(),
-        lr=training_args.learning_rate
+        lr=args.learning_rate
     )
 
     scheduler = get_cosine_schedule_with_warmup(
         optimizer,
-        num_warmup_steps=training_args.warmup_steps,
-        num_training_steps=training_args.num_train_epochs * int(len(ds_train) * 1.0 / training_args.per_device_train_batch_size / training_args.gradient_accumulation_steps)
+        num_warmup_steps=training_args.num_train_epochs * int(len(ds_train) * args.warmup_ratio / training_args.per_device_train_batch_size / training_args.gradient_accumulation_steps),
+        num_training_steps=training_args.num_train_epochs * int(len(ds_train) * 1.0 / training_args.per_device_train_batch_size / training_args.gradient_accumulation_steps),
+        num_cycles=args.num_cycles
     )
 
     trainer = CustomTrainer(
         model=model,
         args=training_args,
         tokenizer=tokenizer,
-        data_collator=DataCollator(),
+        data_collator=data_collator,
         train_dataset=ds_train,
         eval_dataset=ds_val,
         compute_metrics=compute_metrics,
