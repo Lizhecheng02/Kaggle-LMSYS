8811dddc5179d22690730ad0a97918bf4f6fd780
unknown <2569618533@qq.com>
2024-07-17 00:14:31 -0700

    tree first commit

---
+++ tree/config.py
@@ -0,0 +1,12 @@
+class CFG:
+    # tf-idf vectorizer
+    tfidf_vectorizer_ngram_range = (2, 3)
+    tfidf_vectorizer_min_df = 0.10
+    tfidf_vectorizer_max_df = 0.90
+    tfidf_vectorizer_max_features = 1000
+
+    # count vectorizer
+    count_vectorizer_ngram_range = (2, 3)
+    count_vectorizer_min_df = 0.10
+    count_vectorizer_max_df = 0.90
+    count_vectorizer_max_features = 1000
